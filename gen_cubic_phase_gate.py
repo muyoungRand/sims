e9f7dfab6ec545752b7e8d8ee8ca8be98ac7f70f
POPOS <kimmuyoung.97@gmail.com>
2024-04-29 18:46:43 +0800

    Update cubic phase gate simulations. Geting more clues as how to optimise.

--- gen_cubic_phase_gate.py
+++ gen_cubic_phase_gate.py
@@ -6,13 +6,12 @@ from scipy import special
 from scipy.optimize import minimize, shgo
 
 t = 0.5 # Arbitrary time units
-N = 20 # Number of Fourier coefficients
+N = 14 # Number of Fourier coefficients
 param = 1 # Amount of Squeezing
 
 # Optimal parameters for N = 21 based on the paper
 phi_ans = [-0.353282, 0.902028, -0.518804, -0.447977, 0.447404, 0.231963, -0.307409, -0.191352, 0.354135, 0.141061, -0.544457, -0.310069, 0.685625, 0.623302, -0.144266, -0.808851, -0.102101, -0.682909, 1.02161, 0.534188, -0.54781]
 phi = [np.pi/2 for i in range(N + 1)]
-#ls_phi = [2 * np.pi* i for i in ls_phi]
 
 #%%
 # Find all possible combinations of [+1, -1] for lists of length N.
@@ -29,7 +28,7 @@ def airy(t, n):
     """
     Approximation solution to space intgeral
 
-    Error in Paper: Term needs to be multipled by 1/sqrt(pi) to be the approximate integration value
+    Error 1: Term needs to be multipled by 1/sqrt(pi) to be the approximate integration value
     """
     beta = t * n
     return 4.691 * np.exp(-beta/3) * special.airy(0.231 - 0.693 * beta)[0] / np.sqrt(np.pi)
@@ -37,8 +36,6 @@ def airy(t, n):
 def terms(j, sj_list, phi_list):
     """
     Generate each term within the product
-
-    Verified.
     """
     power = (sj_list[j+1] - sj_list[j])/2
 
@@ -57,7 +54,7 @@ def func(phi_list, full_list):
         for i in range(len(ls) - 1):
             #print(terms(i, ls, phi_list))
             inter.append(terms(i, ls, phi_list))
-        exponents.append(np.sum(ls[1:-1]))
+        exponents.append(np.sum(ls[1:-1])+3.5)
         components.append(np.prod(inter))
 
     approx_integral = [airy(t, i) for i in exponents]
@@ -75,7 +72,52 @@ def func(phi_list, full_list):
 
 root = minimize(func, [np.random.uniform(-np.pi/2, np.pi/2) for i in range(N+1)], args = (full_list))
 print(root.message)
-print(root.x)
-print(func(root.x, full_list))
+print("Solution for Optimal Angles: ", root.x)
+print("Calculated infidelity: ", np.round(func(root.x, full_list), 3))
 
- # %%
+# %%
+# To Show Error 1 
+""" import scipy.integrate as integrate
+
+def exact(x, beta):
+    return (1/np.sqrt(np.pi)) * np.exp(-x**2) * np.exp(-1j * x**3) * np.exp(1j * beta * x)
+
+def approx(beta):
+    return 4.691 * np.exp(-beta/3) * special.airy(0.231 - 0.693 * beta)[0] / np.sqrt(np.pi)
+
+trial = [0.1*i for i in range(1,11)]
+
+res_exact = []
+res_approx = []
+for k in trial:
+    res_exact.append(integrate.quad(exact, -np.inf, np.inf, args = k)[0])
+    res_approx.append(approx(k))
+
+plt.plot(trial, res_exact, label = 'Numerical Integration')
+plt.plot(trial, res_approx, label = 'Approximate Airy Function with 1/np.sqrt(pi)')
+plt.legend() """
+# %%
+# Generate Fourier Coefficients
+import scipy.integrate as integrate
+
+def integrand(x, index, time):
+    return np.exp(1j * 1 * x**3) * np.exp(-1j * index * time * x)
+
+def coeff(time, index):
+    norm = time / (2 * np.pi)
+    res = np.real(integrate.quad(integrand, -np.pi/time, np.pi/time, args = (index, time))[0])
+    return norm * res
+
+time = [0.1 * i for i in range(1, 20)]
+
+check = []
+for i in range(100):
+    check.append(coeff(0.42, i))
+
+plt.plot(np.absolute(check), 'x-')
+plt.hlines(y = np.absolute(check[10]), xmin = 0, xmax = 100, color = 'r')
+plt.ylabel("Fourier Amplitude (Absolute Value)")
+plt.xlabel("Fourier Index")
+plt.title("Time = 0.42")
+
+# %%
