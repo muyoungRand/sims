ade4287d3b8b6a745c35276c2fa1cb2320675af0
POPOS <kimmuyoung.97@gmail.com>
2024-09-18 15:06:17 +0800

    Try carriereless simulations

--- testBed.py
+++ testBed.py
@@ -7,7 +7,7 @@ from qutip import *
 
 # ------ SIMULATION & PLOT SETTINGS ------ #
 # Highest Fock state
-nMax = 100 
+nMax = 20 
 
 # Size of Wigner function to plot
 sizeWigner = 5  #Higher numbers require higher nMax for accurate simulations
@@ -33,14 +33,13 @@ pulse_seq = [-0.353282, 0.902028, -0.518804,
             0.685625, 0.623302, -0.144266,
             -0.808851, -0.102101, -0.682909,
             1.021610, 0.534188, -0.54781]
-#%%
+
 def plotter(rho, title):
     motion = ptrace(rho, 1)
-    qubit = ptrace(rho, 0)
-    eigenstate1 = [np.round(qubit.eigenstates()[1][0][0][0][0],3), np.round(qubit.eigenstates()[1][0][1][0][0],3)]
-    eigenstate2 = [np.round(qubit.eigenstates()[1][1][0][0][0],3), np.round(qubit.eigenstates()[1][1][1][0][0],3)]
-
+    nBar = np.sum([i * np.real(np.diag(motion)[i]) for i in range(nMax)])
     W = wigner(motion, xvec, xvec)
+
+    qubit = ptrace(rho, 0)
 
     fig, ax = plt.subplots(1, 1, figsize=(5, 5))
 
@@ -51,61 +50,34 @@ def plotter(rho, title):
     ax.set_xlabel("Momentum, p")
 
     im = ax.contourf(xvec, xvec, W, 100, norm=mpl.colors.Normalize(-.25,.25), cmap=plt.get_cmap('RdBu'))
-    ax.text(-4.0, 4.0, eigenstate1)
-    ax.text(-4.0, 3.5, eigenstate2)
+    ax.text(-4.0, 4.0, np.round(qubit[:],3))
+    ax.text(-4.0, -4.0, 'nBar ='+str(np.round(nBar, 3)))
     ax.set_title(title)
     
     return motion, qubit
 
-def hamiltonian(rabi_freq, phase_RSB, phase_BSB):
-    p = phase_RSB + phase_BSB
-    m = phase_RSB - phase_RSB
-    
-    part1 = sM.dag() * np.exp(1j * p) + sM * np.exp(-1j * p)
-    part2 = a * np.exp(1j * m) + a.dag() * np.exp(-1j * m)
-    
-    return rabi_freq * part1 * part2
+def displace(phase):
+    spin = np.cos(phase) * sX - np.sin(phase) * sY
+    motion = x
 
-rabi_freq = 0.5
-
-phase_BSB = np.pi/2
-phase_RSB = np.pi/2
-
+    return spin * motion
+#%%
 r0 = tensor(basis(2, 0) + basis(2, 1), basis(nMax, 0))
 
-H1 = pulse_seq[0] * sY
-H2 = hamiltonian(rabi_freq, phase_RSB, phase_BSB)
+phase = 2 * np.pi * np.random.rand()
+print(np.round((phase * 180/np.pi)/np.pi, 3))
 
-r1 = mesolve(H = H1, rho0 = r0, tlist = [0, 1], options = options).states[-1]
-m1, q1 = plotter(r1, title = 'After C1')
+H = displace(phase * 2)
+r = mesolve(H = -H, rho0 = r0, tlist = [0, 1], options = options).states[-1]
+m, q = plotter(r, title = 'CarrierLess')
 
+H1 = -sY
+H2 = sX * x
+H3= sY
+
+r1 = mesolve(H = H1, rho0 = r0, tlist = [0, phase], options = options).states[-1]
 r2 = mesolve(H = H2, rho0 = r1, tlist = [0, 1], options = options).states[-1]
-m2, q2 = plotter(r2, title = 'After C1 + D1')
+r3 = mesolve(H = H3, rho0 = r2, tlist = [0, phase], options = options).states[-1]
+m3, q3 = plotter(r3, title = 'Ideal')
 
-# %%
-def func(x):
-    rabi_freq = x[0]
-    phase_RSB = x[1]
-    phase_BSB = x[2]
-    
-    r0 = tensor(basis(2, 0) + basis(2, 1), basis(nMax, 0))
-    H = hamiltonian(rabi_freq, phase_RSB, phase_BSB)
-    
-    r = mesolve(H = H, rho0 = r0, tlist = [0, 1], options = options).states[-1]
-    
-    m = ptrace(r, 1)
-    nBar = np.sum([[i * np.diag(np.real(m))[i] for i in range(nMax)]])
-    
-    q = ptrace(r, 0)
-    eig1 = np.real(q.eigenstates()[1][0][0][0][0])
-    eig2 = np.real(q.eigenstates()[1][0][1][0][0])
-    
-    return nBar - 0.25
-
-from scipy.optimize import minimize
-
-bnds = ( (0.0, 2.0), (0, 2 * np.pi), (0, 2* np.pi) )
-
-res = minimize(func, (0.5, 0, np.pi), bounds=bnds, tol = 1E-6)
-print(res)
 # %%
