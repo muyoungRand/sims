092c523c530ea377fccd95f7d8bf1b3ac0f9cbe6
POPOS <kimmuyoung.97@gmail.com>
2024-12-16 16:34:04 +0800

    MS gate up to working condition

--- sims_backend.py
+++ sims_backend.py
@@ -4,10 +4,12 @@ import qutip as qt
 import scipy.special as special
 
 class operations():
-    def __init__(self, nIons, nModes, nMax):
+    def __init__(self, nIons, nModes, nMax, rabi, eta):
         self.nIons = nIons
         self.nModes = nModes
         self.nMax = nMax
+        self.rabi = rabi
+        self.eta = eta
         
         # Define Operators
         self.sM = []
@@ -65,9 +67,8 @@ class operations():
         return sM
 
     def def_sZ_operator(self, index):
-        sZ = qt.qeye(1) # Don't know how to initialise empty object, so do this, then trace away
+        sZ = qt.qeye(1)
         
-        # Define the sigmaMinus operator for the specific qubit labelled by 'index'
         for i in range(self.nIons):
             if i < index:
                 sZ = qt.tensor(sZ, qt.qeye(2))
@@ -75,8 +76,7 @@ class operations():
                 sZ = qt.tensor(sZ, qt.sigmaz())
             elif i > index:
                 sZ = qt.tensor(sZ, qt.qeye(2))
-                
-        # Tensor away initial 1-array        
+                        
         sZ = qt.ptrace(sZ, [1 + i for i in range(self.nIons)])
         
         for i in range(self.nModes):
@@ -102,13 +102,13 @@ class operations():
         return a
     
     def def_carrier_hamiltonian(self, index):
-        op = self.sM[index].dag()
+        op = self.rabi * self.sM[index].dag()
         def op_coeff(t, args):
-            return np.exp(1j * args['detuning_ion' + str(index)] * t) * np.exp(-1j * args['phase_ion' + str(index)])
-        
-        op_p = self.sM[index]
-        def op_p_coeff(t, args):
             return np.exp(-1j * args['detuning_ion' + str(index)] * t) * np.exp(1j * args['phase_ion' + str(index)])
+        
+        op_p = op.dag()
+        def op_p_coeff(t, args):
+            return np.conjugate(op_coeff(t, args))
         
         carrier = [
             [op, op_coeff], [op_p, op_p_coeff]
@@ -117,13 +117,15 @@ class operations():
         return carrier
 
     def def_rsb_hamiltonian(self, index_ion, index_mode):
-        op = 1j * self.sM[index_ion].dag() * self.a[index_mode]
+        op = (self.rabi / 2) * self.sM[index_ion].dag() * self.a[index_mode]
         def op_coeff(t, args):
-            return np.exp(1j * (args['detuning_rsb_ion' + str(index_ion)] - args['freq_mode' + str(index_mode)]) * t) * np.exp(-1j * args['phase_ion' + str(index_ion)])
+            exp_freq = np.exp(-1j * t * (args['freq_mode' + str(index_mode)] + args['detuning_rsb_ion' + str(index_ion)]))
+            exp_phase = np.exp(1j * args['phase_ion' + str(index_ion)])
+            return exp_freq * exp_phase
             
-        op_p = -1j * self.sM[index_ion] * self.a[index_mode].dag()
+        op_p = op.dag()
         def op_p_coeff(t, args):
-            return np.exp(-1j * (args['detuning_rsb_ion' + str(index_ion)] - args['freq_mode' + str(index_mode)]) * t) * np.exp(1j * args['phase_ion' + str(index_ion)])
+            return np.conjugate(op_coeff(t, args))
         
         rsb = [
             [op, op_coeff], [op_p, op_p_coeff]
@@ -132,17 +134,58 @@ class operations():
         return rsb
     
     def def_bsb_hamiltonian(self, index_ion, index_mode):
-        op = 1j * self.sM[index_ion].dag() * self.a[index_mode].dag()
+        op = (self.rabi / 2) * self.sM[index_ion].dag() * self.a[index_mode].dag()
         def op_coeff(t, args):
-            return np.exp(1j * (args['detuning_bsb_ion' + str(index_ion)] + args['freq_mode' + str(index_mode)]) * t) * np.exp(-1j * args['phase_ion' + str(index_ion)])
+            exp_freq = np.exp(1j * t * (args['freq_mode' + str(index_mode)] - args['detuning_bsb_ion' + str(index_ion)]))
+            exp_phase = np.exp(1j * args['phase_ion' + str(index_ion)])
+            return exp_freq * exp_phase
             
-        op_p = -1j * self.sM[index_ion] * self.a[index_mode]
+        op_p = op.dag()
         def op_p_coeff(t, args):
-            return np.exp(-1j * (args['detuning_bsb_ion' + str(index_ion)] + args['freq_mode' + str(index_mode)]) * t) * np.exp(1j * args['phase_ion' + str(index_ion)])
+            return np.conjugate(op_coeff(t, args))
             
         bsb = [
             [op, op_coeff], [op_p, op_p_coeff]
         ]
         
         return bsb
-# %%
+
+
+# ------------ Example - MS Gate ------------
+
+# Initialise class
+nIons = 2
+nModes = 1
+nMax = 100
+rabi = 2 * np.pi * 0.5
+eta = 0.1
+
+op = operations(nIons, nModes, nMax, rabi, eta)
+
+# System parameters
+freq_mode = 0
+detuning = 1.0
+args = {
+    'freq_mode0': 2 * np. pi * freq_mode,
+
+    "detuning_ion0": 0.0,
+    "detuning_rsb_ion0": -2 * np.pi * detuning,
+    "detuning_bsb_ion0": 2 * np.pi * detuning,
+    "phase_ion0": 0.0,
+    
+    "detuning_ion1": 0.0,
+    "detuning_rsb_ion1": -2 * np.pi * detuning,
+    "detuning_bsb_ion1": 2 * np.pi * detuning,
+    'phase_ion1': 0.0
+}
+
+# Hamiltonians
+carrier_ion0 = op.def_carrier_hamiltonian(index = 0)
+bsb_ion0_mode0 = op.def_bsb_hamiltonian(index_ion = 0, index_mode = 0)
+rsb_ion0_mode0 = op.def_rsb_hamiltonian(index_ion = 0, index_mode = 0)
+
+carrier_ion1 = op.def_carrier_hamiltonian(index = 1)
+bsb_ion1_mode0 = op.def_bsb_hamiltonian(index_ion = 1, index_mode = 0)
+rsb_ion1_mode0 = op.def_rsb_hamiltonian(index_ion = 1, index_mode = 0)
+
+H = bsb_ion0_mode0 + rsb_ion0_mode0 + bsb_ion1_mode0 + rsb_ion1_mode0
